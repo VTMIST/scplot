fdb80820ac1be12b44c06ee02cbaed39cf071f2d
SEC-VT <shanec1@vt.edu>
2017-11-15 10:55:50 -0500

    aligned, added datetimes to the plots

--- scplot.py
+++ scplot.py
@@ -1,23 +1,29 @@
 # This script will plot whatever search coil data is in the current folder.
-# TODO: Put datetimes on the x axis
-#       Label the plots
-#       Adjust the timeseries so they line up with the specgrams
-#       Modularize, yada yada
+# TODO: Modularize, yada yada
 
 import os
 import gzip
 import numpy as np
+import datetime as dt
 import matplotlib.pyplot as plt
-from mpl_toolkits.axes_grid1 import make_axes_locatable
+import matplotlib.dates as mdates
 
 ##############################################################################
 # IMPORT
 
-zip_list = [s for s in os.listdir() if s.endswith('.dat.gz')]
+scpath = './'
+sc_zip_list = [s for s in os.listdir(scpath) if s.endswith('.dat.gz')]
 hex = b''
-for file in zip_list:
-    with gzip.open(file, mode='rb') as bitstream:
-        hex = hex + bitstream.read()
+datetimes = []
+sample_rate = dt.timedelta(microseconds=100000)
+for file in sc_zip_list:
+    file_start = dt.datetime.strptime(file[3:22], '%Y_%m_%d_%H_%M_%S')
+    with gzip.open(scpath + file, mode='rb') as bitstream:
+        in_bits = bitstream.read()
+        hex = hex + in_bits
+        in_dates = [file_start + sample_rate *
+                    x for x in range(0, len(in_bits) // 3)]
+        datetimes = datetimes + in_dates
 
 ##############################################################################
 # PROCESS
@@ -49,31 +55,52 @@ y_samples = y_samples * (.0049 / 4.43)
 ##############################################################################
 # PLOTS
 
-f, axs = plt.subplots(4)
+hours = mdates.HourLocator(interval=2)
+hourFmt = mdates.DateFormatter('%H:00')
 
-axs[0].plot(x_samples)
-axs[0].axis([0, 864000, -0.25, 0.25])
+fig = plt.figure()
+ax0 = fig.add_subplot(411, label="Xspec")
+ax1 = fig.add_subplot(412, label="Xseri")
+ax2 = fig.add_subplot(413, label="Yspec")
+ax3 = fig.add_subplot(414, label="Yseri")
+ax01 = fig.add_subplot(411, label="Xspec2", frame_on=False)
+ax21 = fig.add_subplot(413, label="Yspec2", frame_on=False)
 
-axs[1].specgram(x_samples, NFFT=1024, noverlap=256 * 3, Fs=10,
-                cmap='terrain', scale='dB')
-for im in axs[1].get_images():
-    im.set_clim(-60, 0)
-divider = make_axes_locatable(axs[1])
-cax = divider.append_axes("right", "2%", pad="1%")
-plt.colorbar(im, cax=cax)
+s, f, t, im = ax0.specgram(x_samples, NFFT=1024, noverlap=256 * 3, Fs=10,
+                           cmap='terrain', scale='dB')
+im.set_clim(-60, 0)
+ax0.set_xticklabels([])
+# divider = make_axes_locatable(axs[1])
+# cax = divider.append_axes("right", "2%", pad="1%")
+# plt.colorbar(im, cax=cax)
 
-plt.tight_layout()
+ax01.plot(datetimes, x_samples, 'None')
+ax01.set_yticklabels([])
+ax01.xaxis.set_major_locator(hours)
+ax01.xaxis.set_major_formatter(hourFmt)
 
-axs[2].plot(y_samples)
-axs[2].axis([0, 864000, -0.25, 0.25])
+ax1.plot(datetimes, x_samples)
+ax1.margins(0, 0)
+ax1.xaxis.set_major_locator(hours)
+ax1.xaxis.set_major_formatter(hourFmt)
 
-axs[3].specgram(y_samples, NFFT=1024, noverlap=256 * 3, Fs=10,
-                cmap='terrain', detrend='linear', scale='dB')
-for im in axs[3].get_images():
-    im.set_clim(-60, 0)
-divider = make_axes_locatable(axs[3])
-cax = divider.append_axes("right", "2%", pad="1%")
-plt.colorbar(im, cax=cax)
+s, f, t, im = ax2.specgram(y_samples, NFFT=1024, noverlap=256 * 3, Fs=10,
+                           cmap='terrain', scale='dB')
+im.set_clim(-60, 0)
+ax2.set_xticklabels([])
+# divider = make_axes_locatable(axs[3])
+# cax = divider.append_axes("right", "2%", pad="1%")
+# plt.colorbar(im, cax=cax)
+
+ax21.plot(datetimes, x_samples, 'None')
+ax21.set_yticklabels([])
+ax21.xaxis.set_major_locator(hours)
+ax21.xaxis.set_major_formatter(hourFmt)
+
+ax3.plot(datetimes, y_samples)
+ax3.margins(0, 0)
+ax3.xaxis.set_major_locator(hours)
+ax3.xaxis.set_major_formatter(hourFmt)
 
 plt.show()
 
